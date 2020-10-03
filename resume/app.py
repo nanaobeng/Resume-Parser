988643a48723c190ecca6dd83e75c8f0dba8347a
nanaobeng <nanaobengmarnu@gmail.com>
2020-10-03 12:13:17 +0000

    resume

--- resume/app.py
+++ resume/app.py
@@ -219,7 +219,7 @@ def upload_file():
         return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
 
 
 
